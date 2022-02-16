aed6325011785e557ee39d054ccb0636ac4959f0
Floriane Hibelot <floriane@wemaintain.com>
2022-02-16 15:04:04 +0100

    restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # get 'reviews/index'
+  # get 'reviews/show'
+  # get 'reviews/new'
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding..."
+
+Restaurant.create([{name: "Epicure", category: "french", address:"paris 10"},
+{name: "Djawa", category: "italian", address:"paris canal 10"},
+{name: "Bulle", category: "french", address:"paris 18"},
+{name: "Le Valmy", category: "belgian", address:"paris 10"},
+{name: "La Casrne", category: "japanese", address:"paris 11"}
+])
+puts "created #{Restaurant.count} restaurants!"
+puts "Seeding done."
